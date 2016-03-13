9e668bc727e6178daf4bbc199ecb6a1a4313910c
vagabond1-1983 <vagabond1_1983@163.com>
2016-03-13 21:18:45 +0800

    倒序的第二种写法

--- src/main/java/com/test/basic/string/ReverseString.java
+++ src/main/java/com/test/basic/string/ReverseString.java
@@ -12,5 +12,14 @@ public class ReverseString {
         for (int i = testString.length() - 1; i >= 0; i--) {
             System.out.print(testString.charAt(i));
         }
+
+
+        //第二种方法：不用charAt方法
+        char[] charArray = testString.toCharArray();
+        System.out.println();
+        System.out.println("倒序输出结果为：");
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            System.out.print(charArray[i]);
+        }
     }
 }
