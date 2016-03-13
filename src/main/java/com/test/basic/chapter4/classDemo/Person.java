3c4e36157656713cdc141605201dfeea0186b853
vagabond1-1983 <vagabond1_1983@163.com>
2016-03-13 20:42:23 +0800

    update

--- src/main/java/com/test/basic/chapter4/classDemo/Person.java
+++ src/main/java/com/test/basic/chapter4/classDemo/Person.java
@@ -4,7 +4,7 @@ package com.test.basic.chapter4.classDemo;
  * Created by beigui on 2016/3/8.
  */
 public class Person {
-    String name;
-    int age;
-    String gender;
+    String name;   //姓名
+    int age;       //年龄
+    String gender; //性别 “男”，“女”
 }
