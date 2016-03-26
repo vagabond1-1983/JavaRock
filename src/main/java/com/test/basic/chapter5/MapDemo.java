e1da8201101b10551fe84dcb78cef03b3ed85409
vagabond1-1983 <vagabond1_1983@163.com>
2016-03-26 15:26:32 +0800

    update

--- src/main/java/com/test/basic/chapter5/MapDemo.java
+++ src/main/java/com/test/basic/chapter5/MapDemo.java
@@ -9,9 +9,11 @@ import java.util.Set;
  * 功能：简单介绍HashMap的使用
  * 向一个Map集合中插入元素并根据key的值打印集合中的元素
  */
+@Deprecated
 public class MapDemo {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<String, String>();
+        //key 是1,2,3，给人一个错觉，好像key是下标一样。不恰当的例子。请看MapDemo2
         map.put("1", "apple");
         map.put("2", "pear");
         map.put("3", "orange");
